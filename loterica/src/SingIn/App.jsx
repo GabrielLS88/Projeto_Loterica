65012d6f079cc339ed3ef166a854529c3655e5cf
Gabriel <lopes.biel2588@gmail.com>
2024-04-24 20:31:48 -0300

    Atualização 3

--- loterica/src/SingIn/App.jsx
+++ loterica/src/SingIn/App.jsx
@@ -14,22 +14,21 @@ function SingIn() {
 
   return (
    <div className='bodySingIn'>
-
     <div className="ladoLogin">
-      <div className="divTitulo">
+      <div className="divTituloLogin">
         <h1 id='tituloLadoLogin'>Gestão De Lotérica</h1>
       </div>
       <div className="boxLogin">
         <h2 id='tituloBoxLogin'>LOGIN</h2>
-        <input className='inputSing' type="text" placeholder='Id' />
-        <input className='inputSing' type="text" placeholder='Usuário' />
-        <div className="divInputPassword">
-          <input className='inputSingPassoword'  type={passwordVisivel ? "text" : "password"} placeholder='Senha' /><i id='btnVizualizarPassword' class="bi bi-eye-fill" onClick={converteParaTexto}></i>
+        <input className='inputSingLogin' type="text" placeholder='Id' />
+        <input className='inputSingLogin' type="text" placeholder='Usuário' />
+        <div className="divInputPasswordLogin">
+          <input className='inputSingPassowordLogin'  type={passwordVisivel ? "text" : "password"} placeholder='Senha' /><i id='btnVizualizarPassword' class="bi bi-eye-fill" onClick={converteParaTexto}></i>
         </div>
-        <button className='btnEntrar'>Entrar</button>
-        <div className="blocoLinks">
-          <a id='linksBoxLoguin1' href="#">Esqueceu a senha?</a>
-          <a id='linksBoxLoguin2' href="/singup">Novo Cliente?</a>
+        <button className='btnEntrarLogin'>Entrar</button>
+        <div className="blocoLinksLogin">
+          <a id='linksBoxLogin1' href="#">Esqueceu a senha?</a>
+          <a id='linksBoxLogin2' href="/singup">Novo Cliente?</a>
         </div>
       </div>
     </div>
