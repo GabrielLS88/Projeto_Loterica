baa9ad2074ef2040a5885423635a2f710ea47f87
Gabriel <lopes.biel2588@gmail.com>
2024-04-24 15:27:41 -0300

    Versiao 2

--- loterica/src/SingIn/App.jsx
+++ loterica/src/SingIn/App.jsx
@@ -1,19 +1,36 @@
 import { useState } from 'react'
 import './App.css'
 
+
+
+
 function SingIn() {
+
+  const [passwordVisivel, setPasswordVisivel] = useState(false);
+  
+  const converteParaTexto = () =>{
+    setPasswordVisivel(!passwordVisivel);
+  }
+
   return (
    <div className='bodySingIn'>
 
     <div className="ladoLogin">
-      <h1 id='tituloInicial'>Gestão de lotérica</h1>
+      <div className="divTitulo">
+        <h1 id='tituloLadoLogin'>Gestão De Lotérica</h1>
+      </div>
       <div className="boxLogin">
-        <h2>Loguin</h2>
-        <form>
-          <input type="text" placeholder="Usuário" />
-          <input type="password" placeholder="Senha" />
-          <button>Entrar</button>
-        </form>
+        <h2 id='tituloBoxLogin'>LOGIN</h2>
+        <input className='inputSing' type="text" placeholder='Id' />
+        <input className='inputSing' type="text" placeholder='Usuário' />
+        <div className="divInputPassword">
+          <input className='inputSingPassoword'  type={passwordVisivel ? "text" : "password"} placeholder='Senha' /><i id='btnVizualizarPassword' class="bi bi-eye-fill" onClick={converteParaTexto}></i>
+        </div>
+        <button className='btnEntrar'>Entrar</button>
+        <div className="blocoLinks">
+          <a id='linksBoxLoguin1' href="#">Esqueceu a senha?</a>
+          <a id='linksBoxLoguin2' href="/singup">Novo Cliente?</a>
+        </div>
       </div>
     </div>
 
